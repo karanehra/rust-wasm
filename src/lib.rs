194724b4812f98dcaad9a338e1a0ab848a1bacc4
Karan Nehra <karanehra@gmail.com>
2020-05-22 23:43:14 +0530

    Added a neighbour struct method

--- src/lib.rs
+++ src/lib.rs
@@ -35,4 +35,22 @@ impl Universe {
   fn get_index(&self, row: u32, column: u32) -> usize {
     (row * self.width + column) as usize
   }
+
+  fn live_neighbour_count(&self, row: u32, column: u32) -> u8 {
+    let mut count = 0;
+
+    for curr_row in [self.height, 0, 1].iter().cloned() {
+      for curr_col in [self.width, 0, 1].iter().cloned() {
+        if curr_row == 0 && curr_col == 0 {
+          continue;
+        }
+        let neighbour_row = (row + curr_row) % self.height;
+        let neighbour_col = (column + curr_col) % self.width;
+
+        let idx = self.get_index(neighbour_row, neighbour_col);
+        count += self.cells[idx] as u8
+      }
+    }
+    count
+  }
 }
