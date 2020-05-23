6cfc4b08c1f2ae8ef99e459c8dc5178f76fc76a0
Karan Nehra <karanehra@gmail.com>
2020-05-23 22:54:43 +0530

    Added more methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use std::fmt;
 use wasm_bindgen::prelude::*;
 
 #[cfg(feature = "wee_alloc")]
@@ -52,5 +53,68 @@ impl Universe {
       }
     }
     count
+  }
+
+  pub fn tick(&mut self) {
+    let mut next = self.cells.clone();
+
+    for row in 0..self.height {
+      for col in 0..self.width {
+        let idx = self.get_index(row, col);
+        let cell = self.cells[idx];
+        let live_neighbors = self.live_neighbour_count(row, col);
+
+        let next_cell = match (cell, live_neighbors) {
+          (Cell::Alive, x) if x < 2 => Cell::Dead,
+          (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+          (Cell::Alive, x) if x > 3 => Cell::Dead,
+          (Cell::Dead, 3) => Cell::Alive,
+          (otherwise, _) => otherwise,
+        };
+
+        next[idx] = next_cell;
+      }
+    }
+
+    self.cells = next;
+  }
+
+  pub fn new() -> Universe {
+    let width = 64;
+    let height = 64;
+
+    let cells = (0..width * height)
+      .map(|i| {
+        if i % 2 == 0 || i % 7 == 0 {
+          Cell::Alive
+        } else {
+          Cell::Dead
+        }
+      })
+      .collect();
+
+    Universe {
+      width,
+      height,
+      cells,
+    }
+  }
+
+  pub fn render(&self) -> String {
+    self.to_string()
+  }
+}
+
+impl fmt::Display for Universe {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    for line in self.cells.as_slice().chunks(self.width as usize) {
+      for &cell in line {
+        let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+        write!(f, "{}", symbol)?;
+      }
+      write!(f, "\n")?;
+    }
+
+    Ok(())
   }
 }
