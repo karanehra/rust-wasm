70efcc1fd367afc9cf83d7d0838dc435411113c4
Karan Nehra <karanehra@gmail.com>
2020-05-24 23:06:55 +0530

    Setup wasm pack

--- bindings/wasm_test.js
+++
@@ -1,2 +0,0 @@
-import * as wasm from "./wasm_test_bg.wasm";
-export * from "./wasm_test_bg.js";
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import("./bindings/wasm_test").then((module) => {
   let uni = module.Universe.new();
   const pre = document.createElement("pre", {});
+
   pre.style.lineHeight = "0.9";
   pre.id = "prepre";
   document.body.appendChild(pre);
-  renderLoop();
   const renderLoop = () => {
     pre.textContent = uni.render();
     uni.tick();
     requestAnimationFrame(renderLoop);
   };
+  renderLoop();
 });
