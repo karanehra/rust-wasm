41a57c2ae118326f6bc331b04858839b7d733ef3
Karan Nehra <karanehra@gmail.com>
2020-05-27 23:44:17 +0530

    Modified dirname. Added js bridge using image data

---
+++ js-src/index.js
@@ -0,0 +1,22 @@
+import { Colors } from "wasm-test";
+import { memory } from "wasm-test/wasm_test_bg";
+
+let a = Colors.new();
+let colors = new Uint32Array(memory.buffer, a.get_set_ptr(), 4);
+
+/**
+ * @type {HTMLCanvasElement}
+ */
+
+let canvas = document.getElementById("main");
+canvas.width = 64;
+canvas.height = 64;
+let ctx = canvas.getContext("2d");
+
+let newImageData = ctx.createImageData(64, 64);
+let newImageDataBuffer = new Uint32Array(newImageData.data.buffer);
+
+a = "12";
+
+newImageDataBuffer.set(colors);
+ctx.putImageData(newImageData, 0, 0);
