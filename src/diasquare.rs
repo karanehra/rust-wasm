a9b0f291da235700b80079765fa47066967a3556
Karan Nehra <karanehra@gmail.com>
2020-05-29 23:29:15 +0530

    Fixed immutability issues

--- src/diasquare.rs
+++ src/diasquare.rs
@@ -1,13 +1,23 @@
+use math::round::ceil;
 use rand::prelude::*;
+use wasm_bindgen::prelude::*;
 
-struct DiaSquare {
-  width: u32,
-  height: u32,
+#[wasm_bindgen]
+pub struct DiaSquare {
+  size: u32,
   data: Vec<u32>,
 }
 
+#[wasm_bindgen]
 impl DiaSquare {
-  pub fn new(&self, size: u32) -> DiaSquare {
+  pub fn new(&mut self, size: u32) -> DiaSquare {
+    let finalSize: u32;
+    if size % 2 == 0 {
+      finalSize = size + 1;
+    } else {
+      finalSize = size;
+    }
+
     let mut data = Vec::new();
 
     for _ in 0..size * size {
@@ -19,22 +29,34 @@ impl DiaSquare {
     let bottom_left = randomizer();
     let bottom_right = randomizer();
 
+    /*
+     * Setting corners
+     */
     self.set_cell(0, 0, top_left);
+    self.set_cell(0, size, bottom_left);
+    self.set_cell(size, 0, top_right);
+    self.set_cell(size, size, bottom_right);
+
+    let center = (top_left + top_right + bottom_left + bottom_right) / 4;
+
+    /*
+     * Setting center
+     */
+    self.set_cell(size / 2, size / 2, center);
 
     DiaSquare {
-      width: size,
-      height: size,
+      size: finalSize,
       data,
     }
   }
 
   fn get_idx(&self, x: u32, y: u32) -> usize {
-    (x + self.width * y) as usize
+    (x + self.size * y) as usize
   }
 
-  fn set_cell(&self, x: u32, y: u32, v: u32) {
-    let mut data = self.data.to_vec();
-    data[self.get_idx(x, y)] = v;
+  fn set_cell(&mut self, x: u32, y: u32, v: u32) {
+    let idx = self.get_idx(x, y);
+    self.data[idx] = v;
   }
 
   fn get_cell(&self, x: u32, y: u32) -> u32 {
