37797cfc5a6b6a6624d6fa01d06ebf23f40b1189
Karan Nehra <karanehra@gmail.com>
2020-06-02 23:31:08 +0530

    Changes to randomizer function

--- src/whiteNoise.rs
+++ src/whiteNoise.rs
@@ -1,5 +1,6 @@
 use rand::prelude::*;
 use wasm_bindgen::prelude::*;
+// use std::prelude:
 
 #[wasm_bindgen]
 pub struct WhiteNoise {
@@ -13,8 +14,10 @@ pub struct WhiteNoise {
 impl WhiteNoise {
   pub fn new(size: u32) -> WhiteNoise {
     let mut pixels = Vec::new();
-    for _ in 0..size * size {
-      pixels.push(randomizer());
+    for i in 0..size * size {
+      let x = i - (i / size);
+      let y = i / size;
+      pixels.push(randomizer(x, y));
     }
     WhiteNoise {
       size,
@@ -34,15 +37,21 @@ impl WhiteNoise {
   }
 }
 
-fn randomizer() -> u32 {
-  let mut a: u32 = random::<u32>();
-  let b = a & 0xFF;
-  let g = (a >> 8) & 0xFF;
-  let r = (a >> 16) & 0xFF;
-  a = (b + g + r) / 3 & 0xFF;
-  // a = ((a << 16) | (a << 8) | a) | 0xFF000000;
-  if a > 0x9f {
-    return 0xFFFFFFFF;
-  }
-  return 0xFF000000;
+// fn randomizer() -> u32 {
+//   let mut a: u32 = random::<u32>();
+//   let b = a & 0xFF;
+//   let g = (a >> 8) & 0xFF;
+//   let r = (a >> 16) & 0xFF;
+//   a = (b + g + r) / 3 & 0xFF;
+//   // a = ((a << 16) | (a << 8) | a) | 0xFF000000;
+//   if a > 0x9f {
+//     return 0xFFFFFFFF;
+//   }
+//   return 0xFF000000;
+// }
+
+fn randomizer(x: u32, y: u32) -> u32 {
+  let mut a: f64 = x as f64;
+  let mut b: f64 = y as f64;
+  return (a.sin().abs() * b.cos().abs() * (0xFFFFFFE as f64)) as u32 | 0xFF000000;
 }
