341d6bc2c139c7c41d656b9bcfc15e15662411b8
Karan Nehra <karanehra@gmail.com>
2020-06-05 23:12:07 +0530

    Shifted to three js

--- js-src/index.js
+++ js-src/index.js
@@ -68,6 +68,30 @@
 
 // render();
 
-import main from "./webgl";
+import * as Three from "three";
 
-window.onload = main;
+let scene = new Three.Scene();
+let camera = new Three.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+let renderer = new Three.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+let geo = new Three.BoxGeometry();
+let material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+let cube = new Three.Mesh(geo, material);
+scene.add(cube);
+
+camera.position.z = 3;
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
