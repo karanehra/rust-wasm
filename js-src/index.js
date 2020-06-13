9102dd53a8602d1e3674171d0c58c35eb36c39f1
Karan Nehra <karanehra@gmail.com>
2020-06-14 04:18:20 +0530

    Fixed stutter

--- js-src/index.js
+++ js-src/index.js
@@ -2,10 +2,16 @@ import { WhiteNoise, Map } from "wasm-test";
 import { memory } from "wasm-test/wasm_test_bg";
 import Player from "./player";
 
-let DATA_SIZE = 32;
-let CELL_SIZE = 20;
+let SCALE_FACTOR = 1;
 
-let gameMap = Map.create(DATA_SIZE, 10, CELL_SIZE);
+let DATA_SIZE = 32 / SCALE_FACTOR;
+let CELL_SIZE = Math.floor(window.innerHeight / DATA_SIZE) * SCALE_FACTOR;
+let PLAYER_SIZE = 10 * SCALE_FACTOR;
+let GRAVITY = 1 * SCALE_FACTOR;
+let FLY_SPEED = -0.8 * SCALE_FACTOR;
+let MOVE_SPEED = 0.99 * SCALE_FACTOR;
+
+let gameMap = Map.create(DATA_SIZE, PLAYER_SIZE, CELL_SIZE);
 gameMap.populate();
 
 let canvas = document.getElementById("main");
@@ -19,6 +25,11 @@ gameCanvas.height = DATA_SIZE * CELL_SIZE;
 let ctx = canvas.getContext("2d");
 let mapCtx = gameCanvas.getContext("2d");
 let isMapBeingSetup = false;
+let img = new Image(CELL_SIZE, CELL_SIZE);
+img.src = "./sprites/image_part_001.png";
+img.onload = () => {
+  setupMap();
+};
 
 const setupMap = () => {
   isMapBeingSetup = true;
@@ -28,32 +39,27 @@ const setupMap = () => {
     DATA_SIZE * DATA_SIZE
   );
   mapCtx.clearRect(0, 0, DATA_SIZE * CELL_SIZE, DATA_SIZE * CELL_SIZE);
-  let img = new Image(CELL_SIZE, CELL_SIZE);
-  img.src = "./sprites/image_part_001.png";
-  img.onload = () => {
-    for (let i = 0; i < DATA_SIZE ** 2; i++) {
-      let yCoordinate = Math.floor(i / DATA_SIZE);
-      let xCoordinate = i - yCoordinate * DATA_SIZE;
-      datum[0] = 0;
-      if (datum[i]) {
-        mapCtx.drawImage(
-          img,
-          xCoordinate * CELL_SIZE,
-          yCoordinate * CELL_SIZE,
-          CELL_SIZE,
-          CELL_SIZE
-        );
-      }
+
+  for (let i = 0; i < DATA_SIZE ** 2; i++) {
+    let yCoordinate = Math.floor(i / DATA_SIZE);
+    let xCoordinate = i - yCoordinate * DATA_SIZE;
+    datum[0] = 0;
+    if (datum[i]) {
+      mapCtx.drawImage(
+        img,
+        xCoordinate * CELL_SIZE,
+        yCoordinate * CELL_SIZE,
+        CELL_SIZE,
+        CELL_SIZE
+      );
     }
     setTimeout(() => (isMapBeingSetup = false), 300);
-  };
+  }
 };
 
-setupMap();
+let player = new Player(ctx, PLAYER_SIZE);
 
-let player = new Player(ctx, 10);
-
-gameMap.set_gravity(1);
+gameMap.set_gravity(GRAVITY);
 
 const render = () => {
   ctx.clearRect(0, 0, DATA_SIZE * CELL_SIZE, DATA_SIZE * CELL_SIZE);
@@ -67,18 +73,17 @@ const render = () => {
 let keyData = {};
 
 const handleControls = () => {
-  console.log(keyData);
   if (keyData.ArrowRight) {
-    gameMap.translate_player(0.9, 0);
+    gameMap.translate_player(MOVE_SPEED, 0);
     gameMap.set_facing_right(true);
   } else if (keyData.ArrowLeft) {
-    gameMap.translate_player(-0.9, 0);
+    gameMap.translate_player(-MOVE_SPEED, 0);
     gameMap.set_facing_right(false);
   }
   if (keyData.ArrowUp) {
-    gameMap.set_gravity(-0.5);
+    gameMap.set_gravity(FLY_SPEED);
   } else {
-    gameMap.set_gravity(1);
+    gameMap.set_gravity(GRAVITY);
   }
   if (keyData[" "]) {
     if (!isMapBeingSetup) {
